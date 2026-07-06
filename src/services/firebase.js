365e5003cde6062e81582d9394a936706fd037bf
wwwrwww65-netizen <wwwrwww65@gmail.com>
2026-07-06 08:44:54 +0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,15 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyABv_xTMP4Od2eyzugAyvUR1mTUBEM0HIk",
+  authDomain: "hash-11e5f.firebaseapp.com",
+  projectId: "hash-11e5f",
+  storageBucket: "hash-11e5f.firebasestorage.app",
+  messagingSenderId: "811879615022",
+  appId: "1:811879615022:web:716eca9c9b1120d70dadbf",
+  measurementId: "G-YKHECJ3ZR4"
 };
 
 // Initialize Firebase
